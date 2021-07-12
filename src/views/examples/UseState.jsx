9030a796cbc174f4b7f3d83c80d1f13976c06be5
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-07-12 00:49:48 -0300

    UseState Hook

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle';
 
 const UseState = (props) => {
+
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState("Initial....");
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+
+            <SectionTitle title="Exemple #01"/>
+
+            <div className="center">
+              <span className="text">{count}</span>
+              <div>
+                <button className="btn" onClick={() => setCount(count + 1)}>
+                    +1
+                </button>
+                <button className="btn" onClick={() => setCount(count - 1)}>
+                    -1
+                </button>
+                <button className="btn" onClick={() => setCount(current => current + 1000)}>
+                    +1000
+                </button>
+                <button className="btn" onClick={() => setCount(0)}>
+                    ZERO
+                </button>
+              </div>
+            </div>
+
+            <SectionTitle title="Exemple #02"/>
+
+            <div className="center">
+                <input 
+                    type="text" 
+                    className="input"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
+                <span>{name}</span>
+            </div>
         </div>
     )
 }
