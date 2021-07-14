7ef76d894e0be62bcd092da97ac55cf191a2f7fc
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-07-14 01:05:02 -0300

    useMemo hook

--- src/views/examples/UseMemo.jsx
+++ src/views/examples/UseMemo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle';
 
@@ -7,17 +7,14 @@ const UseMemo = (props) => {
     const [n1, setN1] = useState(0);
     const [n2, setN2] = useState(0);
     const [n3, setN3] = useState(0);
-    const [result, setResult] = useState(0);
-
-    useEffect(() => {
-        setResult(sum(n1, n2));
-    }, [n1, n2])
-
+    
     const sum = (num1, num2) => {
         const future = Date.now() + 2000;
         while(Date.now() < future) {}
         return num1 + num2;
     }
+
+    const result = useMemo(() => sum(n1, n2), [n1, n2]);
 
     return (
         <div className="UseMemo">
