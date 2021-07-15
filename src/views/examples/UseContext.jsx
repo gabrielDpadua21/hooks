7e3cf7796780519490a32397de745f9a2e439924
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-07-15 00:15:52 -0300

    UseContext + contextApi

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle';
+
+import DataContext from '../../data/DataContext';
 
 const UseContext = (props) => {
+
+    const context = useContext(DataContext);
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+
+            <SectionTitle title="Exemple #01" />
+            <div className="center">
+                <span className="text">{context.text}</span>
+                <span className="text red">{context.number}</span>
+            </div>
         </div>
     )
 }
